149ed6e67c2a49127a43cb1452805181117db44a
Esther Nwachukwu <estynwachukwu2016@gmail.com>
2022-10-29 00:13:43 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div>
+        <span className="avatar">EN</span>
+        <span>tincidunt dui ut</span>
+      </div>
+    </div>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,91 @@
+export default [
+  {
+    id: 1,
+    title: "placerat duis ultricies lacus sed turpis tincidunt",
+    imgUrl: "images/product-1.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    price: "NGN 5,500.00",
+  },
+
+  {
+    id: 2,
+    title: "placerat duis ultricies lacus sed turpis tincidunt",
+    imgUrl: "images/product-2.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    price: "NGN 5,500.00",
+  },
+
+  {
+    id: 3,
+    title: "placerat duis ultricies lacus sed turpis tincidunt",
+    imgUrl: "images/product-3.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    price: "NGN 5,500.00",
+  },
+
+  {
+    id: 4,
+    title: "placerat duis ultricies lacus sed turpis tincidunt",
+    imgUrl: "images/product-4.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    price: "NGN 5,500.00",
+  },
+
+  {
+    id: 5,
+    title: "placerat duis ultricies lacus sed turpis tincidunt",
+    imgUrl: "images/product-5.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    price: "NGN 5,500.00",
+  },
+
+  {
+    id: 6,
+    title: "placerat duis ultricies lacus sed turpis tincidunt",
+    imgUrl: "images/product-6.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    price: "NGN 5,500.00",
+  },
+
+  {
+    id: 7,
+    title: "placerat duis ultricies lacus sed turpis tincidunt",
+    imgUrl: "images/product-7.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    price: "NGN 5,500.00",
+  },
+
+  {
+    id: 8,
+    title: "placerat duis ultricies lacus sed turpis tincidunt",
+    imgUrl: "images/product-8.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    price: "NGN 5,500.00",
+  },
+
+  {
+    id: 9,
+    title: "placerat duis ultricies lacus sed turpis tincidunt",
+    imgUrl: "images/product-9.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    price: "NGN 5,500.00",
+  },
+
+  {
+    id: 10,
+    title: "placerat duis ultricies lacus sed turpis tincidunt",
+    imgUrl: "images/product-10.jpg",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    price: "NGN 5,500.00",
+  },
+];

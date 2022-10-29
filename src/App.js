8e4919da434d3a28c78fee2e17df2656566eeab6
Esther Nwachukwu <estynwachukwu2016@gmail.com>
2022-10-29 20:25:06 +0100

    created StoreItems.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
+import StoreItems from "./components/StoreItems";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <StoreItems />
     </div>
   );
 }

10d7330849224d37abd5bbb08044f67b7bf88921
Esther Nwachukwu <estynwachukwu2016@gmail.com>
2022-10-29 23:55:14 +0100

    created Item.js

--- src/components/StoreItems.js
+++ src/components/StoreItems.js
@@ -1,5 +1,19 @@
 import React from "react";
 
 export default function StoreItems() {
-  return <div>StoreItems</div>;
+  return (
+    <div className="store-body">
+      <div className="top-store-body">
+        <div className="input-wrap">
+          <img className="search" src="images/search.png" alt="" />
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="what are you shopping for?"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
